2e6d7a7a8b60d7abc0b26cead9faf27553b3f664
darten73 <artek73rus@gmail.com>
2018-09-07 14:14:08 +0300

    Update BDFDBED.js

--- BDFDBED.js
+++ BDFDBED.js
@@ -1,8 +1,8 @@
 var lib;
 $.get('https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDFDB.js', function(data) {
-	console.log(typeof data)
+	console.log(typeof data);
 	lib = data.toString().replace(/BDFDB/g, 'BDFDBED').replace('My Support Server: http://bit.ly/DevilBrosHaus or https://discordapp.com/invite/Jx3TjNS', 'Спасибо DevilBro за библиотеку: http://bit.ly/DevilBrosHaus').replace(new RegExp('https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/Plugins/', 'g'), 'https://raw.githubusercontent.com/darten73/BetterPlugins/master/plugins/');
-	console.log(lib.length > 0)
+	console.log(lib.length > 0);
 });
-console.log(lib.toString())
-console.log('Beta lib')
+console.log(lib.toString());
+console.log('Beta lib');
