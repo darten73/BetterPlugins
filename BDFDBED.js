b98ad6c692f2ec2b98e1f720b1ab6efe2cbc253d
darten73 <artek73rus@gmail.com>
2018-09-07 15:43:01 +0300

    Add files via upload

--- BDFDBED.js
+++ BDFDBED.js
@@ -1,11 +1,12 @@
 $.get('https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDFDB.js', function(data) {
 	var lib = data.toString().replace(/BDFDB/g, 'BDFDBED').replace('My Support Server: http://bit.ly/DevilBrosHaus or https://discordapp.com/invite/Jx3TjNS', 'Спасибо DevilBro за библиотеку: http://bit.ly/DevilBrosHaus').replace(new RegExp('https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/Plugins/', 'g'), 'https://raw.githubusercontent.com/darten73/BetterPlugins/master/plugins/');
-	libraryScript = document.querySelector('div[id="DarioLib"]');
+	libraryScript = document.querySelector('body div[id="DarioLib"]');
 	if (libraryScript) libraryScript.remove();
 	libraryScript = document.createElement("div");
 	libraryScript.setAttribute("id", "DarioLib")
 	libraryScript.innerHTML = '<script>'+lib+'</script>';
 	document.body.appendChild(libraryScript);
-	console.log("appended")
+	eval(libraryScript.innerHTML);
+	console.log("appended");
 });
 
