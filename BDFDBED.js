249bc2725892384eb65867f555e20e46af01dd9e
darten73 <artek73rus@gmail.com>
2018-09-07 14:06:25 +0300

    Add files via upload

--- BDFDBED.js
+++ BDFDBED.js
@@ -3,3 +3,4 @@ $.get('https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDFDB.js', functi
 	lib = data.toString().replace(/BDFDB/g, 'BDFDBED').replace('My Support Server: http://bit.ly/DevilBrosHaus or https://discordapp.com/invite/Jx3TjNS', 'Спасибо DevilBro за библиотеку: http://bit.ly/DevilBrosHaus').replace(new RegExp('https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/Plugins/', 'g'), 'https://raw.githubusercontent.com/darten73/BetterPlugins/master/plugins/');
 });
 console.log(lib)
+console.log('Beta lib')
